fd4f3d7fda71b6e9750004aebacb181f0efd014c
Max <49519905+Roixxx@users.noreply.github.com>
2019-11-01 10:17:14 +0500

    added slider dots

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 $(function(){
 
     $('.header__slider').slick({
+        asNavFor: '.slider-dots',
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrow slider-arrow-left" src="img/header/arrow-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrow slider-arrow-right" src="img/header/arrow-right.svg" alt=""></img>',
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
     });
 
   
