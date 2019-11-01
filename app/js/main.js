75cc57dde03ac6647041d45aa177504a8daf7b35
Max <49519905+Roixxx@users.noreply.github.com>
2019-11-01 09:46:52 +0500

    added arrow buttons

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function(){
     $('.header__slider').slick({
         infinite: true,
         fade: true,
+        prevArrow: '<img class="slider-arrow slider-arrow-left" src="img/header/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrow slider-arrow-right" src="img/header/arrow-right.svg" alt=""></img>',
     });
 
   
