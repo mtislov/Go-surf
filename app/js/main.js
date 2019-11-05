ed55e0217fcd7d1e1de0eff48767d737e791c9d4
Max <49519905+Roixxx@users.noreply.github.com>
2019-11-05 14:33:17 +0500

    work on the aside

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,13 @@ $(function(){
         asNavFor: '.slider-dots',
         infinite: true,
         fade: true,
+        rows: 0,
         prevArrow: '<img class="slider-arrow slider-arrow-left" src="img/header/arrow-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrow slider-arrow-right" src="img/header/arrow-right.svg" alt=""></img>',
     });
 
     $('.slider-dots').slick({
+        rows: 0,
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
